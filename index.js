f9788f648a02e56f0a9864eac851f9ca50614d87
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-10-20 09:51:35 -0500

    setting up index.js

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ const cheerio = require("cheerio");
 const express = require("express");
 
 const app = express();
+const url = "https://www.theguardian.com/us";
+
+axios(url).then((response) => {
+  const html = response.data;
+  const view = cheerio.load(html);
+  view(".fc-item__title", html).each(function () {
+    const title = view(this).text();
+    const url = view(this).find("a").attr("href");
+  });
+});
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
