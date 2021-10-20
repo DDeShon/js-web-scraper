ce7b07651c946f435d76a5a092046f057e7f8484
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-10-20 09:58:20 -0500

    setting up index.js

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const express = require("express");
 const app = express();
 const url = "https://www.theguardian.com/us";
 
-axios(url).then((response) => {
-  const html = response.data;
-  const view = cheerio.load(html);
-  view(".fc-item__title", html).each(function () {
-    const title = view(this).text();
-    const url = view(this).find("a").attr("href");
-  });
-});
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const view = cheerio.load(html);
+    const articles = [];
+    view(".fc-item__title", html).each(function () {
+      const title = view(this).text();
+      const url = view(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
